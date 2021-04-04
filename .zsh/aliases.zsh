ca973880bc51a0c9c83104951d70c5ea8d34e294
miya-logger <miya.logger@gmail.com>
2021-04-05 03:08:21 +0900

    fix(aliases.zsh): peco から fzf に変更

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,12 +1,12 @@
-#################################  PECO  #################################
-function peco-history-selection() {
-    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+#################################  fzf  #################################
+function fzf-history-selection() {
+    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | fzf`
     CURSOR=$#BUFFER
     zle reset-prompt
 }
 
-zle -N peco-history-selection
-bindkey '^R' peco-history-selection
+zle -N fzf-history-selection
+bindkey '^R' fzf-history-selection
 
 #################################  CDR  #################################
 # cd after ls
@@ -25,20 +25,20 @@ if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]
     zstyle ':chpwd:*' recent-dirs-file "$HOME/.cache/chpwd-recent-dirs"
 fi
 
-function peco-cdr () {
-    local selected_dir="$(cdr -l | sed -E 's/^[0-9]+ +//' | peco --prompt="cdr >" --query "$LBUFFER")"
+function fzf-cdr () {
+    local selected_dir="$(cdr -l | sed -E 's/^[0-9]+ +//' | fzf --prompt="cdr >" --query "$LBUFFER")"
     if [ -n "$selected_dir" ]; then
         BUFFER="cd ${selected_dir}"
         zle accept-line
     fi
 }
-zle -N peco-cdr
-bindkey '^E' peco-cdr
+zle -N fzf-cdr
+bindkey '^E' fzf-cdr
 
 #################################  ALIAS  #################################
 alias ls='lsd'
 alias tree='lsd --tree'
-alias gcd='cd $(ghq root)/$(ghq list | peco)'
-alias gh='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
+alias gcd='cd $(ghq root)/$(ghq list | fzf)'
+alias gh='hub browse $(ghq list | fzf | cut -d "/" -f 2,3)'
 alias dps='docker ps --format "{{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Command}}\t{{.RunningFor}}"'
-alias de='docker exec -it `dps | peco | cut -f 1` /bin/bash'
+alias de='docker exec -it `dps | fzf | cut -f 1` /bin/bash'
